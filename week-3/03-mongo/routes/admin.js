f3785c45bc53a8f6fa9e616309fb31fd97f69515
6D-pixel <dinesh7rd@gmail.com>
2023-12-23 17:57:25 +0530

    hafe done mongo db assignment

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -2,17 +2,34 @@ const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
+app.use(boduParser.json());
+
 // Admin Routes
-app.post('/signup', (req, res) => {
-    // Implement admin signup logic
+app.post("/signup", (req, res) => {
+    Admin.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.json({
+        msg: 'Admin created successfully'
+    })
 });
 
-app.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+app.post("/courses", adminMiddleware, (req, res) => {
+    Course.create({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image
+    })
+    res.json({
+        msg: 'Course created successfully'
+    })    
 });
 
-app.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+app.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
+  Course.find().then((courses) => res.json(courses))
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -5,10 +5,21 @@ const userMiddleware = require("../middleware/user");
 // User Routes
 app.post('/signup', (req, res) => {
     // Implement user signup logic
+    UserActivation.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.json({
+        message: 'User created successfully'
+    })
 });
 
 app.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course.find()
+      .then(courses => {
+            res.json(courses);
+        })
 });
 
 app.post('/courses/:courseId', userMiddleware, (req, res) => {
